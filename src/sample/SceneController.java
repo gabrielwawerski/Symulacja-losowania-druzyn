4187c51b1e1b3b049250fb040d2c8dc4638c622c
masmix1 <4masmix1@gmail.com>
2017-12-18 16:39:15 +0100

    Fix wrong imports, add fields and onAction methods from fxml

--- src/sample/SceneController.java
+++ src/sample/SceneController.java
@@ -1,22 +1,51 @@
 package sample;
 
 import javafx.fxml.FXML;
-
-import javax.swing.text.html.ListView;
-import java.awt.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class SceneController {
     @FXML
-    private ListView koszyk1;
+    private ListView basket1;
     @FXML
-    private ListView koszyk2;
+    private ListView basket2;
     @FXML
-    private ListView koszyk3;
+    private ListView basket3;
     @FXML
-    private ListView koszyk4;
+    private ListView basket4;
+
+    @FXML
+    private Button drawButton;
+    @FXML
+    private Button quickDrawButton;
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private ListView groupA;
+    @FXML
+    private ListView groupB;
+    @FXML
+    private ListView groupC;
+    @FXML
+    private ListView groupD;
+    @FXML
+    private ListView groupE;
+    @FXML
+    private ListView groupF;
+    @FXML
+    private ListView groupG;
+
 
     @FXML
     protected void handleDrawButton() {
     }
 
+    @FXML
+    protected void handleQuickDrawButton() {
+    }
+
+    @FXML
+    protected void handleResetButton() {
+    }
 }
