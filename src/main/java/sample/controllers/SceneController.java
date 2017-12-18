d0e2af0b00f0c4b58a1e13c5421ac695f4b0ee62
jakwer <jakwer93@op.pl>
2017-12-18 18:31:49 +0100

    Change Java version to 8.

---
+++ src/main/java/sample/controllers/SceneController.java
@@ -0,0 +1,51 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class SceneController {
+    @FXML
+    private ListView basket1;
+    @FXML
+    private ListView basket2;
+    @FXML
+    private ListView basket3;
+    @FXML
+    private ListView basket4;
+
+    @FXML
+    private Button drawButton;
+    @FXML
+    private Button quickDrawButton;
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private ListView groupA;
+    @FXML
+    private ListView groupB;
+    @FXML
+    private ListView groupC;
+    @FXML
+    private ListView groupD;
+    @FXML
+    private ListView groupE;
+    @FXML
+    private ListView groupF;
+    @FXML
+    private ListView groupG;
+
+
+    @FXML
+    protected void handleDrawButton() {
+    }
+
+    @FXML
+    protected void handleQuickDrawButton() {
+    }
+
+    @FXML
+    protected void handleResetButton() {
+    }
+}
