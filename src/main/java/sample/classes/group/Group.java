9279e91590a2c23628d4bfb167a725dddc68ebdf
masmix1 <4masmix1@gmail.com>
2017-12-19 00:19:38 +0100

    Add constructors

--- src/main/java/sample/classes/group/Group.java
+++ src/main/java/sample/classes/group/Group.java
@@ -9,8 +9,20 @@ public class Group {
     public Group() {
     }
 
+    public Group(Team team1, Team team2, Team team3, Team team4) {
+        teamScore[0] = new TeamScore(team1);
+        teamScore[1] = new TeamScore(team2);
+        teamScore[2] = new TeamScore(team3);
+        teamScore[3] = new TeamScore(team4);
+    }
+
     private class TeamScore {
         private Team team;
         private Score score;
+
+        TeamScore(Team team) {
+            this.team = team;
+            score = new Score();
+        }
     }
 }
