85f72330b3f7f5917386adb4a53f7f0d36c3768e
masmix1 <4masmix1@gmail.com>
2017-12-18 18:57:50 +0100

    Add Score class

---
+++ src/main/java/sample/classes/group/score/Score.java
@@ -0,0 +1,34 @@
+package sample.classes.group.score;
+
+public class Score {
+    private int points;
+    private int goalsScored;
+    private int goalsLost;
+
+    public Score() {
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public void setGoalsScored(int goalsScored) {
+        this.goalsScored = goalsScored;
+    }
+
+    public int getGoalsLost() {
+        return goalsLost;
+    }
+
+    public void setGoalsLost(int goalsLost) {
+        this.goalsLost = goalsLost;
+    }
+}
