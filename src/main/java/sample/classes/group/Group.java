40a4d204a885e24f8f1049bbcd044d20ffdb5ec3
masmix1 <4masmix1@gmail.com>
2017-12-19 00:38:50 +0100

    Rename TeamScore class to TeamBundle

--- src/main/java/sample/classes/group/Group.java
+++ src/main/java/sample/classes/group/Group.java
@@ -4,23 +4,23 @@ import sample.classes.group.score.Score;
 import sample.classes.team.Team;
 
 public class Group {
-    private TeamScore[] teamScore;
+    private TeamBundle[] teamBundle;
 
     public Group() {
     }
 
     public Group(Team team1, Team team2, Team team3, Team team4) {
-        teamScore[0] = new TeamScore(team1);
-        teamScore[1] = new TeamScore(team2);
-        teamScore[2] = new TeamScore(team3);
-        teamScore[3] = new TeamScore(team4);
+        teamBundle[0] = new TeamBundle(team1);
+        teamBundle[1] = new TeamBundle(team2);
+        teamBundle[2] = new TeamBundle(team3);
+        teamBundle[3] = new TeamBundle(team4);
     }
 
-    private class TeamScore {
+    private class TeamBundle {
         private Team team;
         private Score score;
 
-        TeamScore(Team team) {
+        TeamBundle(Team team) {
             this.team = team;
             score = new Score();
         }
