789c03a0bf9ca17738f006c68d062b9d5c36874b
masmix1 <4masmix1@gmail.com>
2017-12-18 16:01:42 +0100

    Initial commit

---
+++ src/sample/SceneController.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ListView;
+import java.awt.event.ActionEvent;
+
+public class SceneController {
+    @FXML
+    private ListView koszyk1;
+    @FXML
+    private ListView koszyk2;
+    @FXML
+    private ListView koszyk3;
+    @FXML
+    private ListView koszyk4;
+
+    @FXML
+    protected void handleDrawButton() {
+    }
+
+}
