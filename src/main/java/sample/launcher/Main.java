2722060d530ea033447865b54cb9d3c088a85970
jakwer <jakwer93@op.pl>
2017-12-18 18:54:39 +0100

    Setting frame bounds.

--- src/main/java/sample/launcher/Main.java
+++ src/main/java/sample/launcher/Main.java
@@ -2,17 +2,30 @@ package sample.launcher;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
         Parent root = FXMLLoader.load(getClass().getResource("/forms/Scene.fxml"));
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        final double width = primaryScreenBounds.getMinX();
+        final double height = primaryScreenBounds.getMinY();
+        root.setLayoutX(width);
+        root.setLayoutY(height);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
     }
 
