1e1406e38d924947dbed2e2f2252625843333bec
jakwer <jakwer93@op.pl>
2017-12-19 21:34:16 +0100

    Continue making algorithm of drawing

--- src/main/java/sample/controllers/SceneController.java
+++ src/main/java/sample/controllers/SceneController.java
@@ -6,13 +6,9 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
-import javafx.scene.image.Image;
-import sample.classes.team.Continent;
-import sample.classes.team.Team;
 
 import java.net.URL;
-import java.util.ArrayList;
+import java.sql.Time;
 import java.util.ResourceBundle;
 
 public class SceneController implements Initializable {
@@ -35,46 +31,78 @@ public class SceneController implements Initializable {
     @FXML private ListView<String> groupH;
 
 
-    private ObservableList<String> namesBasket1 = null;
-    private ObservableList<String> namesBasket2 = null;
-    private ObservableList<String> namesBasket3 = null;
-    private ObservableList<String> namesBasket4 = null;
+    private ObservableList<String>[] namesTeamsInBasket = new ObservableList[4]; //names of teams in each basket
+    private ObservableList<String>[] namesTeamsInGroup = new ObservableList[7]; //Array with names of teams in each group
 
+    //should be more universal
+    private final int teamsInBasket = 8;
+    private final int numberOfBaskets = 4;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        initializeTeams();
-        namesBasket1 = FXCollections.observableArrayList("Rosja", "Niemcy", "Brazylia",
+
+        namesTeamsInBasket[0] = FXCollections.observableArrayList("Rosja", "Niemcy", "Brazylia",
                 "Portugalia", "Argentyna", "Belgia", "Polska", "Francja");
-        basket1.setItems(namesBasket1);
+        basket1.setItems(namesTeamsInBasket[0]);
 
-        namesBasket2 = FXCollections.observableArrayList("Hiszpania", "Peru", "Szwajcaria",
+        namesTeamsInBasket[1] = FXCollections.observableArrayList("Hiszpania", "Peru", "Szwajcaria",
                 "Anglia", "Kolumbia", "Meksyk", "Urugwaj", "Chorwacja");
-        basket2.setItems(namesBasket2);
+        basket2.setItems(namesTeamsInBasket[1]);
 
-        namesBasket3 = FXCollections.observableArrayList("Dania", "Islandia", "Kostaryka",
+        namesTeamsInBasket[2] = FXCollections.observableArrayList("Dania", "Islandia", "Kostaryka",
                 "Szwecja", "Tunezja", "Egipt", "Senegal", "Iran");
-        basket3.setItems(namesBasket3);
+        basket3.setItems(namesTeamsInBasket[2]);
 
-        namesBasket4 = FXCollections.observableArrayList("Serbia", "Nigeria", "Australia",
+        namesTeamsInBasket[3] = FXCollections.observableArrayList("Serbia", "Nigeria", "Australia",
                 "Japonia", "Maroko", "Panama", "Korea Południowa", "Arabia Saudyjska");
-        basket4.setItems(namesBasket4);
+        basket4.setItems(namesTeamsInBasket[3]);
+
+
+        // binding ObservableList with names of teams and ListView
+        groupA.setItems(namesTeamsInGroup[0]);
+        groupB.setItems(namesTeamsInGroup[1]);
+        groupC.setItems(namesTeamsInGroup[2]);
+        groupB.setItems(namesTeamsInGroup[3]);
+        groupD.setItems(namesTeamsInGroup[4]);
+        groupE.setItems(namesTeamsInGroup[5]);
+        groupF.setItems(namesTeamsInGroup[6]);
+
     }
 
-    private void initializeTeams() {
-    }
-    
+
     @FXML
     protected void handleDrawButton() {
     }
 
+
+
     @FXML
     protected void handleQuickDrawButton() {
 
-        int teamsInBasket1 = namesBasket1.size();
-        int teamsInBasket2 = namesBasket2.size();
-        int teamsInBasket3 = namesBasket3.size();
-        int teamsInBasket4 = namesBasket4.size();
+        int [] numberOfTeamsInBasket = new int [4];
+
+        for (int i = 0; i < numberOfBaskets ; i++)
+        {
+            numberOfTeamsInBasket[i] = namesTeamsInBasket[i].size();
+        }
+
+        int rand = 0;
+        String nameDrawn = "";
+
+        // next team in basket
+        for (int i = 0; i < teamsInBasket; i++)
+        {
+            //next basket
+            for (int j = 0; j < numberOfBaskets; j++)
+            {
+                rand = (int)(Math.floor(Math.random() * numberOfTeamsInBasket[j]));
+                nameDrawn = namesTeamsInBasket[j].get(rand);
+                //namesTeamsInGroup[i].add(nameDrawn);
+                namesTeamsInBasket[j].remove(rand);
+                numberOfTeamsInBasket[j]--;
+
+            }
+        }
 
     }
 
