edaa11201e9c02cf7c574d07264b8847e802119c
masmix1 <4masmix1@gmail.com>
2017-12-20 22:49:18 +0100

    Apply singleton pattern, add comments and javadocs

--- src/main/java/sample/classes/draw/Draw.java
+++ src/main/java/sample/classes/draw/Draw.java
@@ -1,24 +1,43 @@
 package sample.classes.draw;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import sample.classes.team.Team;
+import sample.controllers.SceneController;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
+import java.util.*;
+// TODO receive ObservableLists to baskets and groups, draw teams and put them in baskets, ?then return?
 /**
- * Needed in order to use variables
- * // TODO receive ObservableLists to baskets and groups, draw teams and put them in baskets, ?then return?
+ * Main class for drawing logic and more.
+ * <br>Remember to first get the instance of a class before using it, using
+ * {@link Draw#getInstance} method.
  */
 public class Draw extends Service<Void> {
     public static ObservableList<String>[] baskets;
     public static ObservableList<String>[] groups;
-    public static ArrayList<Team> teams;
+    public static HashMap<String, Team> teams;
+    public static Draw instance = null;
 
+    private Draw() {
+    }
+
+    public static Draw getInstance() {
+        if (instance == null)
+            instance = new Draw();
+        return instance;
+    }
+
+    /**
+     * Injects fields sent from {@link SceneController} class.
+     * @param _baskets {@link SceneController#observableListBasket}
+     * @param _groups {@link SceneController#observableListGroup}
+     * @param _teams {@link SceneController#teams}
+     */
     public static void injectFields(ObservableList<String>[] _baskets, ObservableList<String>[] _groups,
-                                    ArrayList<Team> _teams) {
+                                    HashMap<String, Team> _teams) {
         baskets = _baskets;
         groups = _groups;
         teams = _teams;
