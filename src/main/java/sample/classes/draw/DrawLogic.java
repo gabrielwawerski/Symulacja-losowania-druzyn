2b850bd228ec5e31b4c0425dd99bbcb1c36dd56b
masmix1 <4masmix1@gmail.com>
2017-12-20 22:21:22 +0100

    Add DrawLogic method

---
+++ src/main/java/sample/classes/draw/DrawLogic.java
@@ -0,0 +1,24 @@
+package sample.classes.draw;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class DrawLogic {
+    private AtomicInteger i = new AtomicInteger(0);
+    private AtomicInteger j = new AtomicInteger(0);
+
+    public void startDraw() {
+        // main loop
+        for ( ; i.get() < Draw.baskets.length; i.addAndGet(1)) {
+            for ( ; j.get() < Draw.groups.length; j.addAndGet(1)) {
+            }
+        }
+    }
+
+    public int getI() {
+        return i.get();
+    }
+
+    public int getJ() {
+        return j.get();
+    }
+}
