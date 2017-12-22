6eaf6d5b8212cad98b686bbfdce1ee9b3313023e
masmix1 <4masmix1@gmail.com>
2017-12-22 18:11:17 +0100

    Refactoring

--- src/main/java/sample/controllers/SceneController.java
+++ src/main/java/sample/controllers/SceneController.java
@@ -1,22 +1,19 @@
 package sample.controllers;
 
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.concurrent.WorkerStateEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import sample.classes.basket.Basket;
-import sample.classes.threads.Draw;
-import sample.classes.threads.tasks.FillBasketTask;
+import sample.classes.threads.FillBasketService;
 import sample.classes.team.Continent;
 import sample.classes.team.Team;
 
 import java.net.URL;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class SceneController implements Initializable {
     // TODO? put baskets and groupsListViews into an array, for later letting the user to choose
@@ -41,40 +38,26 @@ public class SceneController implements Initializable {
 
     private ListView<String>[] basketsListViews;
     private ListView<String>[] groupsListViews;
-
     private Basket[] baskets;
-    private HashMap<Integer, ListView<String>> basketMap;
-
     // arrays for observable lists that bind to baskets and groupsListViews ListView's
     private ObservableList<String>[] observableListBasket;
     private ObservableList<String>[] observableListGroup;
-
     // all teams sorted in ascending order - corresponding to their fifa rank + 1
     // 1st place is always taken by the host
     private ArrayList<Team> teams;
     private Team host;
-
-    private final ExecutorService executor = Executors.newFixedThreadPool(2, r -> {
-        Thread t = new Thread(r);
-        t.setDaemon(true);
-        return t;
-    });
-
     // TODO should be more universal
     public static final int NUMBER_OF_BASKETS = 4; // total amount of baskets for teams
     public static final int MAX_BASKET_CAPACITY = 8; // total amount of teams in a single basket
     public static final int NUMBER_OF_GROUPS = 8;
     public static final int MAX_GROUP_CAPACITY = 4;
-
     // the initial size of the team ArrayList (should be equal to the amount of pre added teams)
     private static final int INITIAL_TEAMS_AMOUNT = 32;
-
     // styles for different button states
     private static final String BUTTON_DISABLED_STYLE = "-fx-base: #444444; -fx-text-fill: #333333;";
     private static final String BUTTON_ENABLED_STYLE = "-fx-base: #000000; -fx-text-fill: #FFFFFF;";
 
-
-    private int i = 0;
+    List<List<Team>> teamsSubLists;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -82,16 +65,15 @@ public class SceneController implements Initializable {
         basketsListViews = new ListView[NUMBER_OF_BASKETS];
         groupsListViews = new ListView[NUMBER_OF_GROUPS];
 
-//        setDefaultTeams();
         makeTeams();
         setHost(new Team("Russia", 65, Continent.EUROPE));
         putHost(getHost());
         sortTeamsByValueDescending();
 
-        basketsListViews[0] = basket1;
-        basketsListViews[1] = basket2;
-        basketsListViews[2] = basket3;
-        basketsListViews[3] = basket4;
+        baskets[0] = new Basket(basket1);
+        baskets[1] = new Basket(basket2);
+        baskets[2] = new Basket(basket3);
+        baskets[3] = new Basket(basket4);
 
         groupsListViews[0] = groupA;
         groupsListViews[1] = groupB;
@@ -102,44 +84,26 @@ public class SceneController implements Initializable {
         groupsListViews[6] = groupG;
         groupsListViews[7] = groupH;
 
-        // TODO delete this
-        // instantiate all ObservableLists for team names in baskets
-//        for (int i = 0; i < NUMBER_OF_BASKETS; i++) {
-//            observableListBasket[i] = FXCollections.observableArrayList();
-//        }
-//
-//        for (int i = 0; i < NUMBER_OF_GROUPS; i++) {
-//            observableListGroup[i] = FXCollections.observableArrayList();
-//        }
+        teamsSubLists = new ArrayList<>(NUMBER_OF_BASKETS);
+        List<Team> subList1 = new ArrayList<>(teams.subList(0, 9));
+        List<Team> subList2 = new ArrayList<>(teams.subList(8, 17));
+        List<Team> subList3 = new ArrayList<>(teams.subList(16, 25));
+        List<Team> subList4 = new ArrayList<>(teams.subList(24, 33));
 
-        for (int i = 0; i < NUMBER_OF_GROUPS; i++) {
-            observableListGroup[i] = FXCollections.observableArrayList();
-        }
-
-
-        // neccessary because arrays don't support generics
-//        List<Team>[] lists = (ArrayList<Team>[]) new ArrayList<?>[NUMBER_OF_BASKETS];
-        List<List<Team>> teams_ = new ArrayList<>();
-
-        teams_.get(0).get(0) = teams.subList(0, 9); // TODO FIGURE OUT HOW TO ADD teams to this god awful list and iterate over it in for few lines down
-        lists[1] = teams.subList(8, 17);
-        lists[2] = teams.subList(16, 25);
-        lists[3] = teams.subList(24, 33);
+        teamsSubLists.add(subList1);
+        teamsSubLists.add(subList2);
+        teamsSubLists.add(subList3);
+        teamsSubLists.add(subList4);
 
         // TODO not quite correct - figure out how to do setOnSucceeded and generally where to initialize FillBasketTask - or maybe swap it to Service or some other thing...
         // lookup how to make an object that encapsulates Task, which returns it's value and can be reseted
-        Team[] teams = new Team[MAX_BASKET_CAPACITY];
-        for ( ; i < NUMBER_OF_BASKETS; i++) {
-            teamsSubLists[i].toArray(teams);
-            FillBasketTask fillBasketTask = new FillBasketTask(baskets[i], teams);
-            fillBasketTask.setOnSucceeded(event
-                    -> observableListBasket[getI()] = fillBasketTask.getValue().getObservableList());
-        }
-    }
 
 
-    private int getI() {
-        return i;
+        // task for assigning teams to baskets
+
+        // task for putting teams to groups
+
+        // task that handles ui events - moving teams
     }
 
     @FXML
@@ -150,23 +114,30 @@ public class SceneController implements Initializable {
     protected void handleQuickDrawButton() throws InterruptedException {
         disableAllDrawButtons(true);
 
-        Draw quickDraw = new Draw();
+        FillBasketService fillBasketService = new FillBasketService(baskets, teamsSubLists);
 
-        quickDraw.setOnSucceeded(new javafx.event.EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent event) {
-                // bind array containing teams in each basket to each basket's ListView
-                basket1.setItems(observableListBasket[0]);
-                basket2.setItems(observableListBasket[1]);
-                basket3.setItems(observableListBasket[2]);
-                basket4.setItems(observableListBasket[3]);
-            }
+        fillBasketService.setOnSucceeded(event -> {
+            baskets[0].getListView().setItems(fillBasketService.getValue());
+            System.out.println("Succeeded");
         });
+
+        fillBasketService.setOnFailed(event -> {
+            System.out.println("Failed.");
+        });
+
+        baskets[0].getListView().itemsProperty().bind(Bindings.createObjectBinding(
+                fillBasketService::getValue));
+        baskets[1].getListView().itemsProperty().bind(Bindings.createObjectBinding(
+                fillBasketService::getValue));
+        baskets[2].getListView().itemsProperty().bind(Bindings.createObjectBinding(
+                fillBasketService::getValue));
+        baskets[3].getListView().itemsProperty().bind(Bindings.createObjectBinding(
+                fillBasketService::getValue));
+        fillBasketService.startDraw();
     }
 
     @FXML
     protected void handleResetButton() {
-        setDefaultTeams();
         cleanLists();
 
         disableAllDrawButtons(false);
@@ -287,8 +258,8 @@ public class SceneController implements Initializable {
     }
 
     private void cleanLists() {
-        for (int i = 0; i < MAX_BASKET_CAPACITY; i++) {
-            observableListGroup[i].clear();
+        for (int i = 0; i < NUMBER_OF_BASKETS; i++) {
+            baskets[i].getListView().getItems().clear(); // TODO basket listviews should be manually coded and not added in fxml?
         }
     }
 
